3edd528a561c412c817fae8da67f9b7cf073d0d0
Giovanni Costagliola <giovanni.costagliola@nexusat.it>
2018-06-07 00:31:48 +0200

    My Image

---
+++ tour/my_image.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct{}
+
+func (Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 255, 255)
+}
+
+func (Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x ^ y), uint8(x ^ y), 200, 100}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
